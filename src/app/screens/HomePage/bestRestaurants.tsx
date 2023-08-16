80e5bb58b3f219bc0a6d07bdbaa33ab4e62512ba
Lucas <xolmirzayevotabek779@gmail.com>
2023-08-16 21:30:10 +0900

    feat: member like best restaurants logic creation

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -12,13 +12,21 @@ import {
 import { Box, Button, Container, Stack } from "@mui/material";
 import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
 import CallIcon from "@mui/icons-material/Call";
-import React from "react";
+import React, { useRef } from "react";
 // REDUX
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { retrieveBestRestaurants } from "../../screens/HomePage/selector";
 import { Restaurant } from "../../../types/user";
 import { serverApi } from "../../../lib/config";
+import { Definer } from "../../../lib/Definer";
+import assert from "assert";
+import MemberApiService from "../../apiServices/memberApiService";
+import { useHistory } from "react-router-dom";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../lib/sweetAlert";
 
 /** REDUX SELECTOR */
 const bestRestaurantsRetriever = createSelector(
@@ -30,9 +38,41 @@ const bestRestaurantsRetriever = createSelector(
 
 export function BestRestaurants() {
   /** INITIALIZATIONS **/
+  const history = useHistory();
   const { bestRestaurants } = useSelector(bestRestaurantsRetriever);
+  const refs: any = useRef([]);
 
-  console.log("bestRestaurants:::", bestRestaurants);
+  /** HANDLERS **/
+  const chosenRestaurantHandler = (id: string) => {
+    history.push(`/restaurant/${id}`);
+  };
+  const goRestaurantsHandler = () => history.push("/restaurant");
+  const targetLikeBest = async (e: any, id: string) => {
+    try {
+      assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
+
+      const memberService = new MemberApiService(),
+        like_result: any = await memberService.memberLikeTarget({
+          like_ref_id: id,
+          group_type: "member",
+        });
+      assert.ok(like_result, Definer.general_err1);
+
+      if (like_result.like_status > 0) {
+        e.target.style.fill = "red";
+        refs.current[like_result.like_ref_id].innerHTML++;
+      } else {
+        e.target.style.fill = "white";
+        refs.current[like_result.like_ref_id].innerHTML--;
+      }
+
+      await sweetTopSmallSuccessAlert("success", 700, false);
+    } catch (err: any) {
+      console.log(`targetLikeBest, ERROR:`, err);
+      sweetErrorHandling(err).then();
+    }
+  };
+
   return (
     <div className="best_restaurant_frame">
       <img
@@ -54,8 +94,9 @@ export function BestRestaurants() {
             {bestRestaurants.map((ele: Restaurant) => {
               const image_path = `${serverApi}/${ele.mb_image}`;
               return (
-                <CssVarsProvider>
+                <CssVarsProvider key={ele._id}>
                   <Card
+                    onClick={() => chosenRestaurantHandler(ele._id)}
                     variant="outlined"
                     sx={{
                       minHeight: "483px",
@@ -82,8 +123,12 @@ export function BestRestaurants() {
                           transform: "translateY(50%)",
                           color: "rgba(0, 0, 0, .4)",
                         }}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
                       >
                         <Favorite
+                          onClick={(e) => targetLikeBest(e, ele._id)}
                           style={{
                             fill:
                               ele?.me_liked && ele?.me_liked[0]?.my_favorite
@@ -148,7 +193,11 @@ export function BestRestaurants() {
                           display: "flex",
                         }}
                       >
-                        <div>{ele.mb_likes}</div>
+                        <div
+                          ref={(element) => (refs.current[ele._id] = element)}
+                        >
+                          {ele.mb_likes}
+                        </div>
                         <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
                       </Typography>
                     </CardOverflow>
--- src/app/screens/HomePage/topRestaurants.tsx
+++ src/app/screens/HomePage/topRestaurants.tsx
@@ -18,7 +18,10 @@ import { createSelector } from "reselect";
 import { retrieveTopRestaurants } from "../../screens/HomePage/selector";
 import { Restaurant } from "../../../types/user";
 import { serverApi } from "../../../lib/config";
-import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../lib/sweetAlert";
 import assert from "assert";
 import { Definer } from "../../../lib/Definer";
 import MemberApiService from "../../apiServices/memberApiService";
@@ -43,7 +46,6 @@ export function TopRestaurants() {
   const chosenRestaurantHandler = (id: string) => {
     history.push(`/restaurant/${id}`);
   };
-
   const targetLikeTop = async (e: any, id: string) => {
     try {
       assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
@@ -62,6 +64,7 @@ export function TopRestaurants() {
         e.target.style.fill = "white";
         refs.current[like_result.like_ref_id].innerHTML--;
       }
+      await sweetTopSmallSuccessAlert("success", 700, false);
     } catch (err: any) {
       console.log(`targetLikeTop, ERROR:`, err);
       sweetErrorHandling(err).then();
@@ -138,6 +141,9 @@ export function TopRestaurants() {
                           bottom: 45,
                           transform: "translateY(50%)",
                           color: "rgba(0, 0, 0, .4)",
+                        }}
+                        onClick={(e) => {
+                          e.stopPropagation();
                         }}
                       >
                         <Favorite
