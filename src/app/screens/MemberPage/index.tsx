7faad0872356590781ed8b123bca25b7faa7fb8a
Lucas <xolmirzayevotabek779@gmail.com>
2023-08-26 04:32:51 +0900

    feat: organize retrieve of memberPage url query data

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch, useLocation } from "react-router-dom";
 import "../../../css/my_page.css";
 import { VisitMyPage } from "./VisitMyPage";
 import { VisitOtherPage } from "./VisitOtherPage";
 
+function useQuery() {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
 export function MemberPage(props: any) {
   const { verifiedMemberData } = props;
+  const query = useQuery();
   let member = useRouteMatch();
-  console.log(member);
+  const chosen_mb_id: string | null = query.get("mb_id") ?? null;
+  const chosen_art_id: string | null = query.get("art_id") ?? null;
+  console.log("QUERY TEST:", query.get("art_id"));
 
   return (
     <div className="restaurant_page">
